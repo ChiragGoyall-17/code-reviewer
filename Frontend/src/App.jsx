d49a92e79e0d494674f24afb006b00ba2c172aac
ChiragGoyall-17 <chiraggoyal985@gmail.com>
2025-08-01 21:05:30 +0530

    change in url

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   },[])
 
   async function reviewCode() {
-    const response = await axios.post(`https://code-reviewer-backend-dv8f.onrender.com`,{code})
+    const response = await axios.post(`https://code-reviewer-backend-dv8f.onrender.com/ai/get-review`,{code})
 
     setReview(response.data)
   }
